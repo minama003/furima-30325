8e6cf6de4e8d113bb9c59981af9d180b353a673f
minama003 <73687141+minama003@users.noreply.github.com>
2021-01-26 12:12:26 +0900

    カテゴリー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,21 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    def create
+      @item = Item.new(Item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  end
+
 
   private
   def message_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_day
+  belongs_to :prefecture
+
+
+  validates :name, :string, presence: true
+  validates :text, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :delivery_day_id, numericality: { other_than: 1 } 
+  validates :delivery_charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
 end
