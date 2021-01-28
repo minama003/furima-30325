3dc3fc227757852c9dacf08fe4c1ff99c3a46c26
minama003 <73687141+minama003@users.noreply.github.com>
2021-01-28 18:36:23 +0900

    販売手数料や販売利益の計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,12 @@
-function post (){
-  console.log("非同期投稿を実装");
-}
-
-window.addEventListener('load', post);
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+      console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+      const profitNumber = document.getElementById("profit")
+    profitNumber.innerHTML = (Math.floor(inputValue * 0.9));
+      console.log(profitNumber);
+  })
+});
