9db76f023020c2d355d5b1c6d59e39f1ea6273d2
minama003 <73687141+minama003@users.noreply.github.com>
2021-02-01 15:42:28 +0900

    実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_day
   belongs_to :prefecture
 
+
   with_options  presence: true do
     validates :name
     validates :text
@@ -17,14 +18,14 @@ class Item < ApplicationRecord
   end
 
   with_options  numericality: { other_than: 0 } do
-    validates :category_id
-    validates :condition_id
-    validates :delivery_day_id
-    validates :delivery_charge_id
-    validates :prefecture_id
+    validates :category
+    validates :condition
+    validates :delivery_day
+    validates :delivery_charge
+    validates :prefecture
   end
 
   validates :price, format: { with: /\A[0-9]+\z/}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999 }
-  
+
 end
